9715335b82e5322159d1860f046dc910d22c652a
xiewang <wanxie@ebay.com>
2021-07-01 10:23:34 +0800

    home page style update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,12 +3,17 @@ import styles from './styles.module.css';
 import cns from 'classnames';
 import request from '../../utils/request';
 import { ListView } from 'antd-mobile';
+import missingImg from '../../assets/missing.jpg';
 
 class Home extends Component {
     constructor(props) {
         super(props);
+        const activities = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+        });
         this.state = {
-            activities: []
+            activities: activities,
+
         };
     }
 
@@ -23,25 +28,71 @@ class Home extends Component {
         }).then((res) => {
             if (res.data.state === 'SUCCESS') {
                 this.setState({
-                    activities: res.data.data.content
+                    activities: this.state.activities.cloneWithRows(res.data.data.content)
                 });
             } 
         });
     }
-    
+
+    _onEndReached() {
+        
+    }
+
+    _footer() {
+        return (
+            <div style={{ padding: 30, textAlign: 'center' }}>
+            {this.state.isLoading ? '加载中' : '到底了'}
+            </div>
+        );
+    }
+
+    _jumpToDetailPage () {
+
+    }
+
     render() {
-       
+        const separator = (sectionID, rowID) => (
+            <div
+              key={`${sectionID}-${rowID}`}
+              style={{
+                backgroundColor: '#F5F5F5',
+                height: 8,
+              }}
+            />
+          );
+        const row = (rowData, sectionID, rowID) => {
+            if (rowData.picLink.indexOf('.com')<0) {
+                rowData.picLink = missingImg
+            }
+            return (
+              <div key={rowID} style={{ padding: '0 15px' }}>
+                <div className={styles.title}>{rowData.activityName}</div>
+                <div className={styles.content}>
+                  <img className={styles.img} src={rowData.picLink  } alt="" />
+                  <div className={styles.contentRight}>
+                    <div className={styles.description}>{rowData.activityName}</div>
+                    <div className={cns(styles.contentLine, styles.buttonLine)}>
+                        <div  className={styles.button} onClick={()=>this._jumpToDetailPage()}><span>马上拼课</span></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          };
         return (
           <div className={styles.container}>
-            
-            {
-                this.state.activities.map(v=>{
-                    return (
-                        <div key={v.activityId}><span>活动：{v.activityName}</span></div>
-                    )
-                })
-            }
-            
+            <ListView
+                ref={el => this.lv = el}
+                dataSource={this.state.activities}
+                renderFooter={() => this._footer()}
+                renderRow={row}
+                renderSeparator={separator}
+                className="am-list"
+                useBodyScroll
+                scrollRenderAheadDistance={500}
+                onEndReached={this._onEndReached.bind(this)}
+                onEndReachedThreshold={10}
+            />
           </div>
         );
       }
