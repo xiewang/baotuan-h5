335d9001274d82aecde456dd5439c2d1b2dc0d61
wanxie <wanxie@ebay.com>
2022-01-21 21:52:05 +0800

    remove debug

--- src/utils/common.js
+++ src/utils/common.js
@@ -4,7 +4,7 @@ import { update } from '../actions/session';
 const weChatSDKInit = () => {
     return new Promise((resolve, reject) => {
         wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: 'wx1425c0f4c2e6e3cd', // 必填，公众号的唯一标识
             timestamp: 1626664720557, // 必填，生成签名的时间戳
             nonceStr: 'Wm3WZYTPz0wzccnW', // 必填，生成签名的随机串
